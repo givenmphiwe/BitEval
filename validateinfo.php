b4a1fc87b8c490a110064eec6467fedef8b4fe28
given <giggslive52@gmail.com>
2020-08-07 10:14:57 +0200

    Section 3

---
+++ validateinfo.php
@@ -0,0 +1,25 @@
+<?php
+$myemail = "given@gmail.com";
+$mypass  = "1234";
+
+if(isset($_POST)['login'])){
+    $email = $_POST['email'];
+    $pass  = $_POST['password'];
+    $rem   = $_POST['remember'];
+    if( $email == $myemail and $pass = $mypass){
+        if( isset($_POST['remember']) ){
+            setcookies('email', $email, time()+60*60*7);
+            setcookies('email', $email, time()+60*60*7);
+        }
+        session_start();
+        $_SESSION['email'] = $email;
+        header("location: profile.html");
+
+    }else{
+        echo "ERROR Please use the provided login details<br> try again <a 
+        href='login.php'";
+    }
+}else{
+    header("location: profile.html");
+}
+?>
